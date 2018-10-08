7b955170a5153c1c190fe9ab429b1eab40c319d5
peterckim <peterckim96@gmail.com>
2018-10-08 13:24:37 -0400

    added schema information to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: recipes
+#
+#   id                  :integer            not null, primary key
+#   name                :string     
+#   instructions        :string
+#   image               :string
+#   created_at          :datetime           not null
+#   updated_at          :datetime           not null
+#
+
+
 class Recipe < ApplicationRecord
     has_many :recipe_ingredients
     has_many :cart_recipes

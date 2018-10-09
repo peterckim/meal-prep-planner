16cb4d0dfcc471e14b1c76bdddd6427d0a136aec
peterckim <peterckim96@gmail.com>
2018-10-09 17:32:21 -0400

    added schema information to controllers

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: ingredients
+#
+#   id                  :integer            not null, primary key
+#   name                :string     
+#   created_at          :datetime           not null
+#   updated_at          :datetime           not null
+#
+
 class IngredientsController < ApplicationController
     before_action :require_login
 
